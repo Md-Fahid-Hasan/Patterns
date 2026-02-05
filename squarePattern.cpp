08327722694c0ca1d5457aacb758efc399f1e0b7
Md Fahid Hasan <mdfahidhasan250834@gmail.com>
2026-02-05 19:20:01 +0600

    Square Pattern program added

---
+++ squarePattern.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <chrono>
+using namespace std;
+
+int main() {
+    
+    int n;
+    cout << "Enter the size of the square pattern: ";
+    cin >> n;
+    string pattern = "";
+    for (int i = 0; i < n; i++) {
+        pattern += to_string(i + 1) + " ";
+    }
+    for (int i = 0; i < n; i++) {
+        cout << pattern << "\n";
+    }
+    return 0;
+}

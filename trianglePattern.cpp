2e647c769a45743058adbb35cf78adac42ad69da
Md Fahid Hasan <mdfahidhasan250834@gmail.com>
2026-02-06 17:41:30 +0600

    Triangle Pattern Program added

---
+++ trianglePattern.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the size of the square pattern: ";
+    cin >> n;
+    for(int i = 1; i <= n; i++) {
+        cout << string(i, '*') << "\n";
+    }
+    return 0;
+}
